91dd5e15ba92d9891352502da85567c1a11bff11
Bridgit <brigitasnajder@gmail.com>
2020-05-20 16:53:25 +0200

    First push

---
+++ LV7/LinearSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV7
+{
+    class LinearSearch : SearchStrategy
+    {
+        public override int Search(double[] array, double number)
+        {
+            int arraySize = array.Length;
+            for(int i=0; i<arraySize; i++)
+            {
+                if (array[i] == number)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ LV7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberSequence sequence = new NumberSequence(new double[] { 8, 2, 1, 7, 6, 5, 4, 3, 10, 9 });
+            NumberSequence sequencebubble = sequence;
+            NumberSequence sequencecomb = sequence;
+            sequence.SetSortStrategy(new SequentialSort());
+            sequencebubble.SetSortStrategy(new BubbleSort());
+            sequencecomb.SetSortStrategy(new CombSort());
+            sequence.Sort();
+            Console.WriteLine("Sequential sort:");
+            Console.WriteLine(sequence.ToString());
+            sequencebubble.Sort();
+            Console.WriteLine("Bubble sort:");
+            Console.WriteLine(sequencebubble.ToString());
+            sequencecomb.Sort();
+            Console.WriteLine("Comb sort:");
+            Console.WriteLine(sequencecomb.ToString());
+
+            sequence.SetSearchStrategy(new LinearSearch());
+            int index=sequence.Search(11);
+            if(index != -1)
+            {
+                Console.WriteLine("Index of given number in the array: " + index);
+            }
+            else
+            {
+                Console.WriteLine("There is not given number in the array!");
+            }
+            int index1 = sequence.Search(10);
+            if (index1 != -1)
+            {
+                Console.WriteLine("Index of given number in the array: " + index1);
+            }
+            else
+            {
+                Console.WriteLine("There is not given number in the array!");
+            }
+        }
+    }
+}
